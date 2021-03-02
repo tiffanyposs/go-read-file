8f55e1f034145311798d0c821068214ea8bda954
Tiffany Poss <tiffanyposs@gmail.com>
2021-03-02 09:54:50 -0600

    update solution

--- main.go
+++ main.go
@@ -2,44 +2,62 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	f := ReadArg()
-	PrintFile(f)
-}
-
-func ReadArg() string {
-	args := os.Args
-	var f string
-
-	if len(args) > 1 {
-		f = os.Args[1]
-	}
-
-	return f
-}
-
-func PrintFile(f string) {
-	result, err := os.Open(f)
+	f, err := os.Open(os.Args[1])
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error", err)
 		os.Exit(1)
 	}
 
-	stats, err := result.Stat()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	b := make([]byte, stats.Size())
-	_, e := result.Read(b)
-	if e != nil {
-		fmt.Println(e)
-		os.Exit(1)
-	}
-
-	fmt.Println(string(b))
+	io.Copy(os.Stdout, f)
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// )
+
+// func main() {
+// 	f := ReadArg()
+// 	PrintFile(f)
+// }
+
+// func ReadArg() string {
+// 	args := os.Args
+// 	var f string
+
+// 	if len(args) > 1 {
+// 		f = os.Args[1]
+// 	}
+
+// 	return f
+// }
+
+// func PrintFile(f string) {
+// 	result, err := os.Open(f)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
+
+// 	stats, err := result.Stat()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
+
+// 	b := make([]byte, stats.Size())
+// 	_, e := result.Read(b)
+// 	if e != nil {
+// 		fmt.Println(e)
+// 		os.Exit(1)
+// 	}
+
+// 	fmt.Println(string(b))
+// }
